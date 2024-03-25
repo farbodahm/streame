939831bf527ca7a8ab46b13139a6bd5e89b523c2
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-03-25 16:21:05 +0100

    feat(tests): add more tests for Filter logic

--- pkg/functions/filter_test.go
+++ pkg/functions/filter_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFilter(t *testing.T) {
+func TestFilter_WithDataFrame_AcceptRelatedRecord(t *testing.T) {
 	input := make(chan types.Record)
 	output := make(chan types.Record)
 	errors := make(chan error)
@@ -30,14 +30,19 @@ func TestFilter(t *testing.T) {
 	})
 
 	// Generate sample data
+	accepted_record := types.Record{
+		Key:   "key2",
+		Value: map[string]string{"first_name": "foobar", "last_name": "random_lastname"},
+	}
 	go func() {
 		input <- types.Record{
 			Key:   "key1",
 			Value: map[string]string{"first_name": "random_name", "last_name": "random_lastname"},
 		}
+		input <- accepted_record
 		input <- types.Record{
-			Key:   "key2",
-			Value: map[string]string{"first_name": "foobar", "last_name": "random_lastname"},
+			Key:   "key3",
+			Value: map[string]string{"first_name": "random_name2", "last_name": "random_lastname2"},
 		}
 	}()
 
@@ -46,7 +51,68 @@ func TestFilter(t *testing.T) {
 	// Assertions
 	assert.Nil(t, err)
 	result := <-output
-	assert.Equal(t, "key2", result.Key)
-	assert.Equal(t, "foobar", result.Value.(map[string]string)["first_name"])
+	assert.Equal(t, result, accepted_record)
 	assert.Equal(t, 0, len(output))
+}
+
+func TestFilter_EqualOperator_AcceptRecord(t *testing.T) {
+	filter := functions.Filter{
+		ColumnName: "first_name",
+		Value:      "foobar",
+		Operator:   functions.EQUAL,
+	}
+	record := types.Record{
+		Key:   "key1",
+		Value: map[string]string{"first_name": "foobar", "last_name": "random_lastname"},
+	}
+
+	res := functions.ApplyFilter(filter, &record)
+	assert.NotNil(t, res)
+	assert.Equal(t, *res, record)
+}
+
+func TestFilter_EqualOperator_RejectRecord(t *testing.T) {
+	filter := functions.Filter{
+		ColumnName: "first_name",
+		Value:      "foobar",
+		Operator:   functions.EQUAL,
+	}
+	record := types.Record{
+		Key:   "key1",
+		Value: map[string]string{"first_name": "random_name", "last_name": "random_lastname"},
+	}
+
+	res := functions.ApplyFilter(filter, &record)
+	assert.Nil(t, res)
+}
+
+func TestFilter_NotEqualOperator_AcceptRecord(t *testing.T) {
+	filter := functions.Filter{
+		ColumnName: "first_name",
+		Value:      "foobar",
+		Operator:   functions.NOT_EQUAL,
+	}
+	record := types.Record{
+		Key:   "key1",
+		Value: map[string]string{"first_name": "random_name", "last_name": "random_lastname"},
+	}
+
+	res := functions.ApplyFilter(filter, &record)
+	assert.NotNil(t, res)
+	assert.Equal(t, *res, record)
+}
+
+func TestFilter_NotEqualOperator_RejectRecord(t *testing.T) {
+	filter := functions.Filter{
+		ColumnName: "first_name",
+		Value:      "foobar",
+		Operator:   functions.NOT_EQUAL,
+	}
+	record := types.Record{
+		Key:   "key1",
+		Value: map[string]string{"first_name": "foobar", "last_name": "random_lastname"},
+	}
+
+	res := functions.ApplyFilter(filter, &record)
+	assert.Nil(t, res)
 }
