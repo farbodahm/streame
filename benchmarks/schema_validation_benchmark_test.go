03d2a89a7e43132327ed207ab19478f06cb2a8df
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-05-16 12:01:40 +0200

    tests: add benchmark for schema validation

---
+++ benchmarks/schema_validation_benchmark_test.go
@@ -0,0 +1,40 @@
+package benchmarks
+
+import (
+	"context"
+	"testing"
+
+	"github.com/farbodahm/streame/pkg/core"
+	. "github.com/farbodahm/streame/pkg/types"
+	"github.com/farbodahm/streame/pkg/utils"
+)
+
+// heavy_schema_validation_stages creates lots of heavy structs
+// to benchmark performance of schema validation
+func heavy_schema_validation_stages(number_of_records int) {
+	input := make(chan Record)
+	output := make(chan Record)
+	errors := make(chan error)
+
+	sdf := core.NewStreamDataFrame(input, output, errors, utils.HeavyRecordSchema())
+
+	go func() {
+		for i := 0; i < number_of_records; i++ {
+			input <- utils.NewHeavyRecord(100)
+		}
+	}()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	sdf.Execute(ctx)
+
+	for i := 0; i < number_of_records; i++ {
+		<-output
+	}
+	cancel()
+}
+
+func BenchmarkSchemaValidation(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		heavy_schema_validation_stages(2000)
+	}
+}
