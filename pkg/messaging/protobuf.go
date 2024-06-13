433ff13a88d816e4ffebe6dbb64f6d96a8d71cbd
Farbod Ahmadian <farbodahmadian2014@gmail.com>
2024-06-13 21:37:05 +0200

    feat: add record data to protocol buffers serializer

--- pkg/messaging/protobuf.go
+++ pkg/messaging/protobuf.go
@@ -4,15 +4,16 @@ import (
 	"fmt"
 
 	"github.com/farbodahm/streame/pkg/types"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
 var ErrConvertingToProtoStruct = "Failed converting to Proto struct, unsupported ColumnType: '%v'"
 
-// RecordToProtoStruct converts data of given record to google.protobuf.Struct
-func RecordToProtoStruct(record types.Record) (structpb.Struct, error) {
+// RecordDataToProtoStruct converts data of given record to google.protobuf.Struct
+func RecordDataToProtoStruct(data types.ValueMap) (structpb.Struct, error) {
 	fields := make(map[string]*structpb.Value)
-	for k, v := range record.Data {
+	for k, v := range data {
 		var pbValue *structpb.Value
 
 		switch v.Type() {
@@ -28,4 +29,19 @@ func RecordToProtoStruct(record types.Record) (structpb.Struct, error) {
 	}
 
 	return structpb.Struct{Fields: fields}, nil
+}
+
+// RecordDataToProtocolBuffers marshals given record to protocol buffers
+func RecordDataToProtocolBuffers(data types.ValueMap) ([]byte, error) {
+	protoStruct, err := RecordDataToProtoStruct(data)
+	if err != nil {
+		return nil, err
+	}
+
+	valueData := structpb.NewStructValue(&protoStruct)
+	recordMessage := &RecordData{
+		Data: valueData,
+	}
+
+	return proto.Marshal(recordMessage)
 }
--- pkg/messaging/protubuf_test.go
+++ pkg/messaging/protubuf_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/farbodahm/streame/pkg/messaging"
 	. "github.com/farbodahm/streame/pkg/types"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
@@ -34,7 +35,7 @@ func (t TestType) Type() ColumnType {
 	return 9999
 }
 
-func TestRecordToProtoStruct_ValidRecord_RecordShouldConvertToProtobufStruct(t *testing.T) {
+func TestRecordDataToProtoStruct_ValidRecord_RecordShouldConvertToProtobufStruct(t *testing.T) {
 	record := Record{
 		Key: "key1",
 		Data: ValueMap{
@@ -44,7 +45,7 @@ func TestRecordToProtoStruct_ValidRecord_RecordShouldConvertToProtobufStruct(t *
 		},
 	}
 
-	result, err := messaging.RecordToProtoStruct(record)
+	result, err := messaging.RecordDataToProtoStruct(record.Data)
 	expected_struct := structpb.Struct{
 		Fields: map[string]*structpb.Value{
 			"first_name": {Kind: &structpb.Value_StringValue{StringValue: "foobar"}},
@@ -57,7 +58,7 @@ func TestRecordToProtoStruct_ValidRecord_RecordShouldConvertToProtobufStruct(t *
 	assert.Equal(t, expected_struct.AsMap(), result.AsMap())
 }
 
-func TestRecordToProtoStruct_RecordWithInvalidType_ReturnErrorConvertingToProtoStruct(t *testing.T) {
+func TestRecordDataToProtoStruct_RecordWithInvalidType_ReturnErrorConvertingToProtoStruct(t *testing.T) {
 	record := Record{
 		Key: "key1",
 		Data: ValueMap{
@@ -67,9 +68,52 @@ func TestRecordToProtoStruct_RecordWithInvalidType_ReturnErrorConvertingToProtoS
 		},
 	}
 
-	result, err := messaging.RecordToProtoStruct(record)
+	result, err := messaging.RecordDataToProtoStruct(record.Data)
 	expected_error := fmt.Sprintf(messaging.ErrConvertingToProtoStruct, "9999")
 
 	assert.EqualError(t, err, expected_error)
 	assert.Equal(t, structpb.Struct{}.Fields, result.Fields)
+}
+
+func TestRecordDataToProtocolBuffers_ValidRecord_RecordMarshalsToProtobuf(t *testing.T) {
+	record := Record{
+		Key: "key1",
+		Data: ValueMap{
+			"first_name": String{Val: "foobar"},
+			"last_name":  String{Val: "random_lastname"},
+			"age":        Integer{Val: 23},
+		},
+	}
+	expected_struct := structpb.Struct{
+		Fields: map[string]*structpb.Value{
+			"first_name": {Kind: &structpb.Value_StringValue{StringValue: "foobar"}},
+			"last_name":  {Kind: &structpb.Value_StringValue{StringValue: "random_lastname"}},
+			"age":        {Kind: &structpb.Value_NumberValue{NumberValue: 23}},
+		},
+	}
+
+	result, err := messaging.RecordDataToProtocolBuffers(record.Data)
+	assert.Nil(t, err)
+
+	var resultDeserialized messaging.RecordData
+	err = proto.Unmarshal(result, &resultDeserialized)
+	assert.Nil(t, err)
+	assert.Equal(t, expected_struct.Fields, resultDeserialized.GetData().GetStructValue().Fields)
+}
+
+func TestRecordDataToProtocolBuffers_RecordWithInvalidType_ReturnErrorConvertingToProtoStruct(t *testing.T) {
+	record := Record{
+		Key: "key1",
+		Data: ValueMap{
+			"first_name": String{Val: "foobar"},
+			"last_name":  String{Val: "random_lastname"},
+			"age":        TestType{Val: 23},
+		},
+	}
+
+	result, err := messaging.RecordDataToProtocolBuffers(record.Data)
+	expected_error := fmt.Sprintf(messaging.ErrConvertingToProtoStruct, "9999")
+
+	assert.EqualError(t, err, expected_error)
+	assert.Nil(t, result)
 }
